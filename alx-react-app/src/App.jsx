121a786efd87f227340bfd3f660dcface3c98528
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-07-10 14:45:41 +0300

    add header ,maincontenet and footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,14 @@
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
