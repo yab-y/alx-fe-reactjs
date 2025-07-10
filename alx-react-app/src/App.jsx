b85e3470703d49fd7e027640cf40391d5b866b98
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-07-10 14:55:51 +0300

    user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography." />
       <Footer />
     </>
   );
