36f4c976a099b824209ae4548cf195dacaeec3cb
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-07-26 20:48:14 +0300

    Added inline CSS styling to components in alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', padding: '15px', textAlign: 'center' }}>
+      <p>&copy; 2025 My City App. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
