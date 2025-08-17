b6950778f92dde720a26b0d8554ae124c2861c57
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-08-17 21:20:13 +0300

    Initial reat +tailwind setup for recipe sharing platform

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
-  darkMode: false,
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // All React components
+    './public/index.html'          // Include public HTML
+  ],
+  darkMode: false, // or 'media' / 'class'
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
