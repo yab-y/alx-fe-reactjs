7e966fae3e62e7a5e97cabbe186fd9ad2b61eafc
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-08-30 22:44:49 +0300

    to do list componentwith jest & react testing library

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TodoList from "./components/TodoList";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <div style={{ padding: "20px" }}>
+      <TodoList />
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
